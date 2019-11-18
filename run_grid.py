fc9e0cc452b7dad30391dbaa8ce889b6434780a4
Calvin Smith <cjsmith@cs.wisc.edu>
2019-11-17 21:51:39 -0600

    feat: first

---
+++ run_grid.py
@@ -0,0 +1,51 @@
+from argparse import ArgumentParser
+from json import load, loads
+from csv import DictWriter
+from itertools import product
+
+import subprocess
+
+parser = ArgumentParser("T R O N")
+parser.add_argument("grid", required=True)
+parser.parse_args()
+
+def exec(cmd):
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for line in result.stdout.split("\n"):
+        try:
+            yield loads(line)
+        except:
+            pass
+
+def get_cmds(grid):
+    prod = [
+        [grid["command"]]
+    ]
+
+    try: arguments = grid["arguments"]
+    except: arguments = []
+
+    for arg in arguments:
+        if "flag" in arg.keys():
+            flag = cmd["flag"]
+            prod.append(
+                [f"-{flag}"]
+            )
+
+            # try and get the values
+            if "values" in arg.keys():
+                values = cmd["values"]
+                prod.append(
+                    [str(v) for v in values]
+                )
+    
+    return product(*prod)
+
+
+if __name__ == "__main__":
+    with open(args.grid, "r") as f:
+        grid = load(f)
+    
+    for cmd in get_cmds(grid):
+        for line in exec(cmd):
+            print(line)
